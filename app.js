5c02964ee30100aa1881976202d10a8660267db5
Jazz OM <jazzaiman@gmail.com>
2013-04-30 14:26:05 +0000

    cleanup, make it work

--- app.js
+++ app.js
@@ -1,12 +1,13 @@
 // Require jojo and create a server
 var jojo = require('jojo'),
-    express = jojo.express,
 // jojo.createServer makes an express server and auto-uses jojo
-    app = jojo.createServer();
+    app = jojo.createServer(),
+    express = jojo.express;
+var path = require('path');
 
 app.listen(8080);
 
 app.set('view engine', 'ejs');
-app.use(express['static'](process.cwd() + '/public'));
+app.use(express.static(path.join(__dirname, 'public')));
 
 console.log('Server is listening to http://127.0.0.1:8080');
