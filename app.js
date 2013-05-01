04daefef95b672fa03fc664f6421e3e74ee4821d
Andrej Jazz Novikov <jazzaiman@gmail.com>
2013-05-01 15:33:14 +0000

    add gith

--- app.js
+++ app.js
@@ -1,4 +1,7 @@
 // Require jojo and create a server
+var sys = require('sys')
+var exec = require('child_process').exec;
+var gith = require('gith').create(8081);
 var jojo = require('jojo'),
 // jojo.createServer makes an express server and auto-uses jojo
     app = jojo.createServer(),
@@ -10,5 +13,16 @@ app.set('view engine', 'jade');
 app.use(express.static(path.join(__dirname, 'public')));
 app.listen(8080);
 
+
+
+
+function puts(error, stdout, stderr) { sys.puts(stdout) }
+exec("git pull", puts);
+
+gith({
+  repo: 'shroomist/jojo'
+}).on( 'all', function( payload ) {
+    console.log('Post Recieve happen' );
+});
 
 console.log('Server is listening to http://127.0.0.1:8080');
